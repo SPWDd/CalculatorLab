a1ed7984810052343c84b87e8147a010fa1a8fef
SUPAWIDA SINGSUTTICHAN <supawida_s@cmu.ac.th>
2018-09-12 15:14:09 +0700

    num butt

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,145 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+                        
+            if (lblDisplay.Text == "0"){
+                lblDisplay.Text = "1";
+            } else{
+                lblDisplay.Text = lblDisplay.Text + "1";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            /*numberBtn_Click("2");*/
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "2";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "3";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "3";
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "4";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "4";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "5";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "5";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "6";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "6";
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "7";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "7";
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "8";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "8";
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "9";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "9";
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "0";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + "0";
+            }
+        }
+
+        void numberBtn_Click(string s)
+        {
+            lblDisplay.Text = s;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
